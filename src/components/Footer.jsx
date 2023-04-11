8cf58e2e3ffa7000c839006ac825b4cb0fdd0e9c
Gab Erves <gerves20@gmail.com>
2023-04-10 20:49:48 -0400

    post styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,6 +4,7 @@ import React from 'react';
 const Footer = () => {
   return (
     <Container className="bg-brown-300 text-3xl font-bold text-center">
+      <div>"Sharing perspectives, inspiring minds: the power of blogging."</div>
       Copyright © 2023, Bulletin Social
     </Container>
   );
